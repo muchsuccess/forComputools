ad4be99965bace559f73e06ea5d08142a6608666
maxim <maxim2000zp@gmail.com>
2022-02-21 19:27:29 +0200

    BasedServer

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-// const express = require( "express" );
+// require("dotenv").config();
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// require("dotenv").config();
+// import { config } from 'dotenv';
 
 import express from "express";
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    res.status(200).send( "Hello world!" );
+} );
+
+app.get( "/echo", ( req, res ) => {
+    res.status(200).send( "Echo" );
+} );
+
+app.all( "*", ( req, res ) => {
+    res.status(404).send( "Error404" );
 } );
 
 // start the Express server
